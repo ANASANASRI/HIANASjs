c4ea23bf6c4fabcb81a445723f58f58bf6c861f5
ANASRIANAS <100525138+ANASRIANAS@users.noreply.github.com>
2022-11-10 15:40:16 +0100

    staged changes

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("mar7ba");
+const express=require("express");
+const app=express();
+
+const route=require("./routes/clientsRoutes")
+
+app.use(express.urlencoded({extended:true}));
+
+app.use("/",route)
+
+app.listen(7000,function(){
+    console.log("Sever is running");
+});
